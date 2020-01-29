20b82d2e9449a2e7f9d8448b5b43afca308e5d29
tjdgus <opei71@naver.com>
2020-01-29 16:16:18 +0900

    routes.js 생성 및 url관리 routes.js에서 url을 모두 관리하여 라우터어디에서나 URL전체를 알 필요 없이 사용이 가능하다 routes.이름

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -20,7 +21,7 @@ app.use(helmet())
 app.use(morgan("dev"));
 //해당 URL이 요청되고 해당하는 함수들이 호출된다. 
 // 라우터를 이용한 URL쪼개기
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 export default app;
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,14 @@
 import express from "express";
+import routes from "../routes";
 
 const globalRouter = express.Router();
+
+//전역으로 보여줄 것들을 글로벌 라우터에서 다루자
+globalRouter.get(routes.home, (req,res) => res.send('Home'))
+globalRouter.get(routes.join, (req,res) => res.send('Join'))
+globalRouter.get(routes.login, (req,res) => res.send('Login'))
+globalRouter.get(routes.logout, (req,res) => res.send('Logout'))
+globalRouter.get(routes.search, (req,res) => res.send('Search'))
+
 
 export default globalRouter;
