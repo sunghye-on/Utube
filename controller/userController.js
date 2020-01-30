bd435d124b68d8c985908e3c0e199f27d7dc8434
tjdgus <opei71@naver.com>
2020-01-30 13:52:09 +0900

    템플릿변수 및 input데이터 처리

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
 // 장고의 view.py와 같은 부분이다 현재는 페이지에 글자만 랜더랑하고 있다.
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,16 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+//렌더할 때 pageTitle을 같이 보낼 수 있다. 혹은 원하는 무엇이라도 같이 보내줄 수 있다.
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => {
+    //console.log(req.query.term);
+    //아래 코드는 query에서 term을 가져 오는 것과 같은 코드이다. 즉 req.query.term와 같은 코드
+    //해당 방법이 term = req.query.term 라고 하는 것보다 더 좋은 방식으로 정보를 가져온다.
+    const {
+        //term을 searchBy로 해준다. 즉 req.query.term === searchBy
+        query : {term : searchBy}  
+    } = req;
+    res.render("search", { pageTitle: "Search", searchBy:searchBy});  
+};
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
