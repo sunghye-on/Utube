c86cf64321e9e3aa5925b715d571ae171f11f59b
tjdgus <opei71@naver.com>
2020-01-31 16:18:36 +0900

    mongoDB first connect

--- db.js
+++ db.js
@@ -1,38 +1,80 @@
-export const videos = [
-    {
-        id: 215632,
-        title: 'test비디오1',
-        description: '테스트용 데이터1',
-        views: 522222,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 7777,
-            name: "sunghyeon",
-            email: "sung@sung.com",
-        }
-    },
-    {
-        id: 142453,
-        title: 'test비디오2',
-        description: '테스트용 데이터2',
-        views: 345346,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 1123,
-            name: "sunghyeon1",
-            email: "sung1@sung.com",
-        }
-    },
-    {
-        id: 689456,
-        title: 'test비디오3',
-        description: '테스트용 데이터3',
-        views: 343,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 1111,
-            name: "sunghyeon2",
-            email: "sung2@sung.com",
-        }
-    },
-]
+import mongoose from "mongoose";
+
+// 몽고db의 연결은 "mongodb://localhost:포트번호/db이름" 이다
+mongoose.connect("mongodb://localhost:27017/utube", {
+    useNewURLParser: true,
+    useFindAndModifiy: false,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleDBOpen = () => console.log("🖐 Open DB!");
+const handleDBError = error => console.log(`🤮 DB Connection ERROR : ${error}`); 
+
+db.once("open", handleDBOpen);
+db.on("error", handleDBError);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const videos = [
+//     {
+//         id: 215632,
+//         title: 'test비디오1',
+//         description: '테스트용 데이터1',
+//         views: 522222,
+//         videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//         creator: {
+//             id: 7777,
+//             name: "sunghyeon",
+//             email: "sung@sung.com",
+//         }
+//     },
+//     {
+//         id: 142453,
+//         title: 'test비디오2',
+//         description: '테스트용 데이터2',
+//         views: 345346,
+//         videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//         creator: {
+//             id: 1123,
+//             name: "sunghyeon1",
+//             email: "sung1@sung.com",
+//         }
+//     },
+//     {
+//         id: 689456,
+//         title: 'test비디오3',
+//         description: '테스트용 데이터3',
+//         views: 343,
+//         videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//         creator: {
+//             id: 1111,
+//             name: "sunghyeon2",
+//             email: "sung2@sung.com",
+//         }
+//     },
+// ]
--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 import app from "./app";
-
+import "./db";
 const PORT = 4000;
 
 const handleListen = () => console.log(`✅ 서버 연결 완료 http://localhost:${PORT}`);
