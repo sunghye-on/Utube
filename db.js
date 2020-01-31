4ec57306639b8677c7f51ad7ba7a2c6df0b1db57
tjdgus <opei71@naver.com>
2020-01-31 16:36:07 +0900

    dotenv를 이용한 정보 숨기기 .env파일을 만들어 숨기고 싶은 정보를 적고 dotenv.config()함수로 해당 내용을 불러올 수 있고 .gitignore로 파일을 숨긴다.

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+//아래의 함수로 .env의 내용들을 가져올 수 있고 그 내용들은 process.env에 저장
+dotenv.config();
 // 몽고db의 연결은 "mongodb://localhost:포트번호/db이름" 이다
-mongoose.connect("mongodb://localhost:27017/utube", {
+mongoose.connect(process.env.MONGO_URL, {
     useNewURLParser: true,
     useFindAndModifiy: false,
     useUnifiedTopology: true
--- init.js
+++ init.js
@@ -1,6 +1,9 @@
 import app from "./app";
 import "./db";
-const PORT = 4000;
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const handleListen = () => console.log(`✅ 서버 연결 완료 http://localhost:${PORT}`);
 
