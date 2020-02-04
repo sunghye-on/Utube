136143e18801b297d39aeff6b7a49233354caba0
tjdgus <opei71@naver.com>
2020-02-04 19:18:44 +0900

    비디오 최신순 정렬 및 eslint설치

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  rules: {
+    "no-console": "off",
+    "spaced-comment": "off"
+  },
+  env: {
+    browser: true
+  }
+};
--- controller/videoController.js
+++ controller/videoController.js
@@ -2,97 +2,95 @@ import routes from "../routes";
 import Video from "../models/video";
 
 //비디로를 찾기위하여 비동기 작업
-export const home = async(req, res) => {
-    try{
-        const videos = await Video.find({});
-        res.render("home", { pageTitle: "Home", videos });
-    }catch(e){
-        console.log(e);
-        res.render("home", { pageTitle: "Home", videos });
-    }
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({}).sort({ createAt: -1 });
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (e) {
+    console.log(e);
+    res.render("home", { pageTitle: "Home", videos });
+  }
 };
 export const search = (req, res) => {
-    //console.log(req.query.term);
-    //아래 코드는 query에서 term을 가져 오는 것과 같은 코드이다. 즉 req.query.term와 같은 코드
-    //해당 방법이 term = req.query.term 라고 하는 것보다 더 좋은 방식으로 정보를 가져온다.
-    const {
-        //term을 searchBy로 해준다. 즉 req.query.term === searchBy
-        query : {term : searchBy}  
-    } = req;
-    res.render("search", { pageTitle: "Search", searchBy:searchBy, videos});  
+  //console.log(req.query.term);
+  //아래 코드는 query에서 term을 가져 오는 것과 같은 코드이다. 즉 req.query.term와 같은 코드
+  //해당 방법이 term = req.query.term 라고 하는 것보다 더 좋은 방식으로 정보를 가져온다.
+  const {
+    //term을 searchBy로 해준다. 즉 req.query.term === searchBy
+    query: { term: searchBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchBy: searchBy, videos });
 };
-export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = async(req, res) => {
-    const {
-        body: {title, description},
-        file: {path}
-    } = req;
-    //비디오 저장 및 업로드
-    const newVideo = await Video.create({
-        fileUrl :path,
-        title: title,
-        description: description
-    });
-    console.log(newVideo);
-    res.redirect(routes.videoDetail(newVideo.id));
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path }
+  } = req;
+  //비디오 저장 및 업로드
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title: title,
+    description: description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = async(req, res) =>{
-    //params를 이용하여 url을 가져올 수 있다. 
-    //routes.js에서 const VIDEO_DETAIL = "/:id"; 에서 :id를 해주었기 때문에 
-    //콘솔에서는 { id: '5e37e54c1bfb1f603444c78d' } 이런식으로 id의 값이 찍힌다
-    // console.log(req.params);
-    const {
-        params:{id}
-    } = req;
-    try{
+export const videoDetail = async (req, res) => {
+  //params를 이용하여 url을 가져올 수 있다.
+  //routes.js에서 const VIDEO_DETAIL = "/:id"; 에서 :id를 해주었기 때문에
+  //콘솔에서는 { id: '5e37e54c1bfb1f603444c78d' } 이런식으로 id의 값이 찍힌다
+  // console.log(req.params);
+  const {
+    params: { id }
+  } = req;
+  try {
     const video = await Video.findById(id);
     // console.log(video);
-    res.render("videoDetail", { pageTitle: video.title ,video });
-    }
-    catch(error){
-        console.log(`ERROR ❌ ${error}`);
-        res.redirect(routes.home);
-    }
-}
-export const getEditVideo = async(req, res) => {
-    const {
-        params:{id}
-    } = req;
-    try{
-        const video = await Video.findById(id);
-        res.render("editVideo", { pageTitle: `edit ${video.title}`, video });
-    } catch(e){
-        console.log(`ERROR ❌ ${e}`);
-        res.redirect(routes.home);
-    }
-}
-
-export const postEditVideo = async(req,res) =>{
-    const {
-        params:{id},
-        body:{title, description}
-    } = req;
-    try{
-        await Video.findOneAndUpdate({ _id: id }, { title, description });
-        res.redirect(routes.videoDetail(id))
-    } catch(e){
-        console.log(`ERROR ❌ ${e}`);
-        res.redirect(routes.home);
-    }
-
+    res.render("videoDetail", { pageTitle: video.title, video });
+  } catch (error) {
+    console.log(`ERROR ❌ ${error}`);
+    res.redirect(routes.home);
+  }
 };
-export const deleteVideo = async(req, res) => {
-    const {
-        params:{id}
-    } = req;
-    try{
-        await Video.findByIdAndDelete({_id:id});
-        console.log("delete 성공!✔");
-        res.redirect(routes.home);
-    } catch(e){
-        console.log("Delete fail");
-        res.redirect(routes.home);
-    } 
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `edit ${video.title}`, video });
+  } catch (e) {
+    console.log(`ERROR ❌ ${e}`);
+    res.redirect(routes.home);
+  }
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+  } catch (e) {
+    console.log(`ERROR ❌ ${e}`);
+    res.redirect(routes.home);
+  }
+};
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    await Video.findByIdAndDelete({ _id: id });
+    console.log("delete 성공!✔");
+  } catch (e) {
+    console.log("Delete fail");
+  }
+  res.redirect(routes.home);
 };
