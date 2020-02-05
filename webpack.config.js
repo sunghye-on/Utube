ae56f67fdcb5466238bb5796c8dd3a0671710c50
tjdgus <opei71@naver.com>
2020-02-05 16:59:26 +0900

    webpack을 이용한 js파일 scss파일연결 및 -w옵션을 이용하여 webpack이  파일들을 지켜봄

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const autoprefixer = require("autoprefixer");
 //코
-const extractCSS = require("extract-text-webpack-plugin");
+const ExtractCSS = require("extract-text-webpack-plugin");
 //웹팩의 모드를 지정한다 이 모드는 실행할때 정한다.
 const MODE = process.env.WEBPACK_ENV;
 //파일들이 어디서 시작되고 어떠한 디렉토리에서 만들어질지 정한다.
@@ -17,9 +17,17 @@ const config = {
     //코드를 찾아내고 이해해서 scss를 css로 바꾸고 호환도록 해줌
     rules: [
       {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
         //테스트라는 이름의 조건(아래에서는 scss파일)을 만나면 use로 플러그인을 사용하여 진행
         test: /\.(scss)$/,
-        use: extractCSS.extract([
+        use: ExtractCSS.extract([
           {
             //webpack이 css를 이해할 수 있게 해준다.
             loader: "css-loader"
@@ -46,7 +54,7 @@ const config = {
     path: OUTPUT_DIR,
     filename: "[name].js"
   },
-  plugins: [new extractCSS("style.css")]
+  plugins: [new ExtractCSS("style.css")]
 };
 
 module.exports = config;
